b44abc5a8ea9eb68b6e59300ebda0fd724fcd452
john <johnmclient.ngujo@cit.edu>
2023-11-20 00:58:18 -0800

    dashboard admin aa

---
+++ src/inside/AdminDashboard.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import "./Navbar.css";
+import "../pages/AdminDashboard.css.css";
+import { Link } from "react-router-dom";
+export default function AdminDashboard() {
+  useEffect(() => {
+    document.body.style.backgroundColor = "#E7EFFF";
+  }, []);
+  return (
+    <>
+    <div className="da"
+    </>
+  )
+}
