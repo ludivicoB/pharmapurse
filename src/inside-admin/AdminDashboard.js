48e01e2c0df9eb975fe16c8bd99d4af9e84f562a
idul <ludivicobalaterojr@gmail.com>
2023-11-20 17:14:53 +0800

    navbar for admin dashboard imported

--- src/inside-admin/AdminDashboard.js
+++ src/inside-admin/AdminDashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "./AdminDashboard.css";
 import "../inside-admin/AdminDashboard.css";
+import NavBar from "../inside/NavBar";
 export default function AdminDashboard() {
   useEffect(() => {
     document.body.style.backgroundColor = "#E7EFFF";
@@ -8,6 +9,7 @@ export default function AdminDashboard() {
   return (
     <>
       <div>
+        <NavBar />
         <h1 className="header">JohnP0gi</h1>
       </div>
     </>
