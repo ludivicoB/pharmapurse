f75e500f4b5bd04af17ded4e5e312d673b442405
ROBENNNN <robennavales15@gmail.com>
2023-11-22 01:18:17 +0800

    PharmaDashboard nabutngan na

--- src/inside-pharma/PharmaDashboard.js
+++ src/inside-pharma/PharmaDashboard.js
@@ -1,15 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./PharmaDashboard.css";
-import { useEffect } from "react";
 import NavBar from "../inside/NavBar";
+
 export default function PharmaDashboard() {
   useEffect(() => {
     document.body.style.backgroundColor = "#E7EFFF";
   }, []);
+
   return (
     <div>
       <NavBar />
-      <h1 className="pharmahome-h1">R0benPogi</h1>
+     
+
+      {/* App Features */}
+      <div className="features">
+        <div className="featurestext">
+          <h2>Pharmacist Commands</h2>
+          <hr />
+          <p>
+          This platform enhances collaboration among pharmacy staff, facilitating seamless communication 
+          and coordination. PharmaPurse represents a significant leap forward in the digitization of pharmacy
+          operations, providing a comprehensive solution for pharmacists to optimize their workflow and deliver exceptional patient care.
+          </p>
+        </div>
+        <div className="fbox-container">
+          <Link to="/customerSupport" className="link-nounder">
+            <div className="fbox">
+              <img
+                className="cutsup"
+                src="/images/cutsup.png"
+                alt="pharmapurse"
+              ></img>
+              <h2 className="fbox-h2">Customer Order Manager</h2>
+              <p>
+                Manage customer orders and handle them efficiently.
+              </p>
+            </div>
+          </Link>
+        </div>
+      </div>
+
+      {/* Pharmacist Hub */}
+      <div className="siteSection2">
+        <img src="/images/homesection2.png" alt="pharmapurse"></img>
+        <div className="left2">
+          <h1>Pharmacist Hub</h1>
+          <p>
+            Welcome to your one-stop digital health destination. Explore our
+            curated catalog, chat with expert pharmacists for instant advice,
+            and simplify medication management with our virtual pillbox. Your
+            well-being starts here—discover, connect, and thrive.
+          </p>
+        </div>
+      </div>
+
+      {/* Footer */}
+      <footer>
+        <div className="footer-container">
+          <div className="navtitle">
+            <img src="/images/navtitle.png" alt="pharmapurse" />
+          </div>
+          <p>
+            PharmaPurse provides progressive, and affordable <br />
+            healthcare, accessible on mobile and online <br /> for everyone
+          </p>
+          <p>©PharmaPurse PTY LTD 2020. All rights reserved</p>
+        </div>
+      </footer>
     </div>
   );
 }
