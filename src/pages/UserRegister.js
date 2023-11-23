dd9ea5ab1e82b9bb781b5fcc1221f6e3427d7cc5
idul <ludivicobalaterojr@gmail.com>
2023-11-23 20:12:47 +0800

    UserRegister now working with api

--- src/pages/UserRegister.js
+++ src/pages/UserRegister.js
@@ -1,10 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../pages/UserRegister.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
 export default function UserRegister() {
+  const [postData, setPostData] = useState({
+    firstname: "",
+    lastname: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setPostData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleRegisterClick = async () => {
+    try {
+      await axios.post("http://localhost:8080/user/insertUser", postData);
+      console.log("Registration successful:", postData);
+      alert("Registration successful, You may now proceed to login");
+      // Handle success (e.g., redirect, show a success message to the user)
+      // window.location.reload();
+    } catch (error) {
+      console.error("Error registering user:", error);
+      // Handle error (e.g., show an error message to the user)
+    }
+  };
+
   useEffect(() => {
     document.body.style.backgroundColor = "#E7EFFF";
+    // console.log(postData);
   }, []);
+
   return (
     <>
       <div className="loginbox-container">
@@ -16,34 +49,54 @@ export default function UserRegister() {
               <input
                 className="usereg-input"
                 type="text"
-                placeholder="name"
+                placeholder="firstname"
+                name="firstname"
+                value={postData.firstname}
+                onChange={handleInputChange}
+              ></input>
+              <br />
+              <input
+                className="usereg-input"
+                type="text"
+                placeholder="lastname"
+                name="lastname"
+                value={postData.lastname}
+                onChange={handleInputChange}
               ></input>
               <br />
               <input
                 className="usereg-input"
                 type="text"
                 placeholder="username"
+                name="username"
+                value={postData.username}
+                onChange={handleInputChange}
               ></input>
               <br />
               <input
                 className="usereg-input"
                 type="text"
                 placeholder="email"
+                name="email"
+                value={postData.email}
+                onChange={handleInputChange}
               ></input>
               <br />
               <input
                 className="usereg-input"
                 type="password"
                 placeholder="password"
+                name="password"
+                value={postData.password}
+                onChange={handleInputChange}
               ></input>
               <br />
-              <input
-                className="usereg-input"
-                type="password"
-                placeholder="confirm password"
-              ></input>
-              <br />
-              <button className="usereg-btnregister">Register</button>
+              <button
+                className="usereg-btnregister"
+                onClick={handleRegisterClick}
+              >
+                Register
+              </button>
               <hr />
               <Link to="/user">
                 <button className="usereg-btnlogin">Login</button>
