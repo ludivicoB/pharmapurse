1ac96151c62e8e97d50685cd08fe78cd07d98945
Jared <jaredmatthew.limalima@cit.edu>
2023-11-23 21:06:55 +0800

    Userlist

--- src/inside-admin/UserList.js
+++ src/inside-admin/UserList.js
@@ -1,11 +1,42 @@
-import React from "react";
-import "./UserList.css";
-import AdminNavbar from "./AdminNavbar";
-export default function UserList() {
+import React, { useState } from 'react';
+import './UserList.css';
+import AdminNavbar from './AdminNavbar';
+
+const UserCustomerList = () => {
+  const [customers, setCustomers] = useState([
+   
+  ]);
+
   return (
-    <>
+    <div className='user-list-container'>
       <AdminNavbar />
-      <div></div>
-    </>
+      <table className='user-list-table'>
+      <div className="gap">
+          <div className="pono">
+            <h1 className="admincom">User/Customer List</h1>
+          </div>
+          </div>
+          <div className="bb">
+            </div>
+       <tbody>
+          {customers.map((customer) => (
+            <tr key={customer.id}>
+              <td>{customer.name}</td>
+              <td>{customer.email}</td>
+              <td>
+                <img
+                  src={customer.avatar}
+                  alt={customer.name}
+                  className='user-avatar'
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    
   );
-}
+};
+
+export default UserCustomerList;
