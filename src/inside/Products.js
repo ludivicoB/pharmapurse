73bade78cef62baa607f2309db6413fe5b443bc2
idul <ludivicobalaterojr@gmail.com>
2023-11-23 19:08:30 +0800

    products page, with axios get from database

--- src/inside/Products.js
+++ src/inside/Products.js
@@ -1,37 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./Products.css";
 import NavBar from "./NavBar";
 import ProductCard from "./singlecomponents/ProductCard";
 import { Link } from "react-router-dom";
 export default function Products() {
-  useEffect(() => {});
-  const products = [
-    {
-      name: "PARACETAMOL Biogesic Tablet 10s",
-      price: "31.25",
-      image: "bio.png",
-    },
-    {
-      name: "SALONSIPMethyl Salicylate Gel Patch x 2 Patches/Pack",
-      price: "91.25",
-      image: "salon.png",
-    },
-    {
-      name: "Calcuimide Calcium + Vitamin D + Mineral 1 Tablet",
-      price: "11.50",
-      image: "cal.png",
-    },
-    {
-      name: "CENTRUM Silver Advance Multivitamins 30 Tablets",
-      price: "365.00",
-      image: "cent.png",
-    },
-    {
-      name: "NORVASC Amlodipine 10mg 30 tablets",
-      price: "21.00",
-      image: "nor.png",
-    },
-  ];
+  const [products, setProduct] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/product/getAllProduct")
+      .then((response) => {
+        // setUsers(response.data.filter(user => user.userid === 6));
+        setProduct(response.data);
+        console.log("response", response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }, []);
+  // const products = [
+  //   {
+  //     name: "PARACETAMOL Biogesic Tablet 10s",
+  //     price: "31.25",
+  //     image: "bio.png",
+  //   },
+  //   {
+  //     name: "SALONSIPMethyl Salicylate Gel Patch x 2 Patches/Pack",
+  //     price: "91.25",
+  //     image: "salon.png",
+  //   },
+  //   {
+  //     name: "Calcuimide Calcium + Vitamin D + Mineral 1 Tablet",
+  //     price: "11.50",
+  //     image: "cal.png",
+  //   },
+  //   {
+  //     name: "CENTRUM Silver Advance Multivitamins 30 Tablets",
+  //     price: "365.00",
+  //     image: "cent.png",
+  //   },
+  //   {
+  //     name: "NORVASC Amlodipine 10mg 30 tablets",
+  //     price: "21.00",
+  //     image: "nor.png",
+  //   },
+  // ];
   return (
     <>
       <NavBar />
