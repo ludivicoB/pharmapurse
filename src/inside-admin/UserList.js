2736159964352accde19ac5a020cae01b5d99a7e
idul <ludivicobalaterojr@gmail.com>
2023-11-23 21:35:23 +0800

    uselist update

--- src/inside-admin/UserList.js
+++ src/inside-admin/UserList.js
@@ -1,41 +1,20 @@
-import React, { useState } from 'react';
-import './UserList.css';
-import AdminNavbar from './AdminNavbar';
+import React, { useState } from "react";
+import "./UserList.css";
+import AdminNavbar from "./AdminNavbar";
 
 const UserCustomerList = () => {
-  const [customers, setCustomers] = useState([
-   
-  ]);
+  // const [customers, setCustomers] = useState([]);
 
   return (
-    <div className='user-list-container'>
+    <>
       <AdminNavbar />
-      <table className='user-list-table'>
-      <div className="gap">
-          <div className="pono">
-            <h1 className="admincom">User/Customer List</h1>
-          </div>
-          </div>
-          <div className="bb">
-            </div>
-       <tbody>
-          {customers.map((customer) => (
-            <tr key={customer.id}>
-              <td>{customer.name}</td>
-              <td>{customer.email}</td>
-              <td>
-                <img
-                  src={customer.avatar}
-                  alt={customer.name}
-                  className='user-avatar'
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-    
+      <div className="user-list-container">
+        <div className="admin-h1-container">
+          <h1 className="admin-h1">User/Customer List</h1>
+          <div className="admin-userlist-bodylist"></div>
+        </div>
+      </div>
+    </>
   );
 };
 
