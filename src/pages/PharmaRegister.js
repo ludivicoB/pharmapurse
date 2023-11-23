f7aeea47445f6ddd3eed404a98caf5043ef86f61
idul <ludivicobalaterojr@gmail.com>
2023-11-23 22:09:20 +0800

    pharma register with api ah yeah

--- src/pages/PharmaRegister.js
+++ src/pages/PharmaRegister.js
@@ -1,10 +1,31 @@
 import React, { useEffect } from "react";
 import "../pages/PharmaRegister.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 export default function PharmaRegister() {
   useEffect(() => {
     document.body.style.backgroundColor = "#E7EFFF";
   }, []);
+
+  const handleRegisterClick = async () => {
+    try {
+      await axios.post("http://localhost:8080/pharmacist/insertPharmacist", {
+        firstname: document.getElementById("firstname").value,
+        lastname: document.getElementById("lastname").value,
+        username: document.getElementById("username").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+      });
+      console.log("Registration successful:");
+      alert("Registration successful, You may now proceed to login");
+      // Handle success (e.g., redirect, show a success message to the user)
+      window.location.reload();
+    } catch (error) {
+      console.error("Error registering user:", error);
+      // Handle error (e.g., show an error message to the user)
+    }
+  };
+
   return (
     <>
       <div className="loginbox-container">
@@ -17,12 +38,14 @@ export default function PharmaRegister() {
                 className="Registerinput"
                 placeholder="firstname"
                 type="text"
+                id="firstname"
               ></input>
               <br />
               <input
                 className="Registerinput"
                 placeholder="lastname"
                 type="text"
+                id="lastname"
               ></input>
               <br />
 
@@ -30,23 +53,31 @@ export default function PharmaRegister() {
                 className="Registerinput"
                 placeholder="username"
                 type="text"
+                id="username"
               ></input>
               <br />
               <input
                 className="Registerinput"
                 placeholder="email"
                 type="text"
+                id="email"
               ></input>
               <br />
               <input
                 className="Registerinput"
                 placeholder="password"
                 type="password"
+                id="password"
               ></input>
               <br />
 
               <br />
-              <button className="pharmaregister-btnregister">Register</button>
+              <button
+                className="pharmaregister-btnregister"
+                onClick={handleRegisterClick}
+              >
+                Register
+              </button>
               <hr />
               <Link to="/pharmacist">
                 <button className="pharmaregister-btnlogin">Login</button>
