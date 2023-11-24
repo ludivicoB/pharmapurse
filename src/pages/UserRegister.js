36dc24beaf7e15b34e788b410051022db2d2ba2f
idul <ludivicobalaterojr@gmail.com>
2023-11-25 00:31:11 +0800

    user register done with form handle

--- src/pages/UserRegister.js
+++ src/pages/UserRegister.js
@@ -1,30 +1,62 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "../pages/UserRegister.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default function UserRegister() {
-  const [postData, setPostData] = useState({
-    firstname: "",
-    lastname: "",
-    username: "",
-    email: "",
-    password: "",
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setPostData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
+  useEffect(() => {
+    document.body.style.backgroundColor = "#E7EFFF";
+    // console.log(postData);
+  }, []);
   const handleRegisterClick = async () => {
+    const firstName = document.getElementById("firstname").value;
+    const lastName = document.getElementById("lastname").value;
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (
+      firstName !== "" &&
+      lastName !== "" &&
+      username !== "" &&
+      email !== "" &&
+      password !== ""
+    ) {
+      if (password.length < 8) {
+        alert("Password must be at least 8 characters.");
+        return;
+      }
+
+      if (
+        !(
+          /[A-Z]/.test(password) &&
+          /[a-z]/.test(password) &&
+          /\d/.test(password) &&
+          /[^\w\d]/.test(password)
+        )
+      ) {
+        alert(
+          "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+        );
+        return;
+      }
+    } else {
+      alert("Please fill in all fields.");
+      return;
+    }
+
     try {
-      await axios.post("http://localhost:8080/user/insertUser", postData);
-      console.log("Registration successful:", postData);
-      alert("Registration successful, You may now proceed to login");
+      // Make the API call to register the user
+      await axios.post("http://localhost:8080/user/insertUser", {
+        firstname: firstName,
+        lastname: lastName,
+        username: username,
+        email: email,
+        password: password,
+      });
+
+      console.log("Registration successful:");
+      alert("Registration successful. You may now proceed to login.");
       // Handle success (e.g., redirect, show a success message to the user)
       window.location.reload();
     } catch (error) {
@@ -32,11 +64,6 @@ export default function UserRegister() {
       // Handle error (e.g., show an error message to the user)
     }
   };
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "#E7EFFF";
-    // console.log(postData);
-  }, []);
 
   return (
     <>
@@ -51,8 +78,7 @@ export default function UserRegister() {
                 type="text"
                 placeholder="firstname"
                 name="firstname"
-                value={postData.firstname}
-                onChange={handleInputChange}
+                id="firstname"
               ></input>
               <br />
               <input
@@ -60,8 +86,7 @@ export default function UserRegister() {
                 type="text"
                 placeholder="lastname"
                 name="lastname"
-                value={postData.lastname}
-                onChange={handleInputChange}
+                id="lastname"
               ></input>
               <br />
               <input
@@ -69,8 +94,7 @@ export default function UserRegister() {
                 type="text"
                 placeholder="username"
                 name="username"
-                value={postData.username}
-                onChange={handleInputChange}
+                id="username"
               ></input>
               <br />
               <input
@@ -78,8 +102,7 @@ export default function UserRegister() {
                 type="text"
                 placeholder="email"
                 name="email"
-                value={postData.email}
-                onChange={handleInputChange}
+                id="email"
               ></input>
               <br />
               <input
@@ -87,8 +110,7 @@ export default function UserRegister() {
                 type="password"
                 placeholder="password"
                 name="password"
-                value={postData.password}
-                onChange={handleInputChange}
+                id="password"
               ></input>
               <br />
               <button
