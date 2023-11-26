36cdd12fc3836146e9be525d3d2252e076539318
idul <ludivicobalaterojr@gmail.com>
2023-11-26 15:41:34 +0800

    landing page - design adjustments

--- src/Home.js
+++ src/Home.js
@@ -15,15 +15,43 @@ export default function Home() {
       <div className="control-container">
         <div className="control-buttons">
           <Link to="/user">
-            <button className="home-btn">User</button>
+            <button className="home-btn">
+              <div className="user-btn">
+                <img
+                  className="user-icon"
+                  src="/images/usericon.png"
+                  alt="pharmapurse"
+                ></img>
+                User
+              </div>
+            </button>
           </Link>
-
+          <br />
           <Link to="/pharmacist">
-            <button className="home-btn">Pharmacist</button>
+            <button className="home-btn">
+              <div className="user-btn">
+                <img
+                  className="user-icon"
+                  src="/images/pharmaicon.png"
+                  alt="pharmapurse"
+                ></img>
+                Pharmacist
+              </div>
+            </button>
           </Link>
+          <br />
 
           <Link to="/admin">
-            <button className="home-btn">Admin</button>
+            <button className="home-btn">
+              <div className="user-btn">
+                <img
+                  className="user-icon"
+                  src="/images/adminicon.png"
+                  alt="pharmapurse"
+                ></img>
+                Admin
+              </div>
+            </button>
           </Link>
         </div>
       </div>
